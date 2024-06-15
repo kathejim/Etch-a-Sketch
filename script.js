db58753c666993581fbf3aaa49d6843bb02933d5
kathejim <147934904+kathejim@users.noreply.github.com>
2024-06-15 13:23:05 -0500

    Adjust the 16x16 grid to fit dynamically into the screen (css)

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-//Create a 16x16 grid of square divs.
 const container = document.querySelector(".container");
 
-const containerFlex = document.createElement("div");
+//Create a 16x16 grid of square divs.
+const containerGrid = document.createElement("div");
+containerGrid.classList.add("container-grid");
 
-container.appendChild(containerFlex);
-for (let i = 0; i < 4; i++) {
+container.appendChild(containerGrid);
+for (let i = 0; i < 16; i++) {
     const containerHorizontal = document.createElement("div");
     containerHorizontal.classList.add("container-horizontal");
-    containerFlex.appendChild(containerHorizontal);
-    for (let j = 0; j < 4; j++) {
+    containerGrid.appendChild(containerHorizontal);
+
+    for (let j = 0; j < 16; j++) {
         const containerVertical = document.createElement("div");
         containerVertical.classList.add("container-vertical");
         containerHorizontal.appendChild(containerVertical);
