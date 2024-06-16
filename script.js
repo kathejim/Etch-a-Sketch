86817720f76b5f3cdf3439acdb6a6e681ebb48ac
kathejim <ksjm88@gmail.com>
2024-06-15 21:37:09 -0500

    Create a function to create the grid with the size giving by the user

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const container = document.querySelector(".container");
 const btnPopup = document.querySelector("button");
 const popupBox = document.querySelector(".popup-box");
-let pixelNumber = "";
+let pixelNumber = 16;
 
-//Create a 16x16 grid of square divs.
+//Create a 16x16 grid of square divs and a function to change the grid size.
 const containerGrid = document.createElement("div");
 containerGrid.classList.add("container-grid");
 
 container.appendChild(containerGrid);
-for (let i = 0; i < 16; i++) {
-    const containerHorizontal = document.createElement("div");
-    containerHorizontal.classList.add("container-horizontal");
-    containerGrid.appendChild(containerHorizontal);
+function createGrid() {
+    for (let i = 0; i < pixelNumber; i++) {
+        const containerHorizontal = document.createElement("div");
+        containerHorizontal.classList.add("container-horizontal");
+        containerGrid.appendChild(containerHorizontal);
 
-    for (let j = 0; j < 16; j++) {
-        const containerVertical = document.createElement("div");
-        containerVertical.classList.add("container-vertical");
-        containerHorizontal.appendChild(containerVertical);
+        for (let j = 0; j < pixelNumber; j++) {
+            const containerVertical = document.createElement("div");
+            containerVertical.classList.add("container-vertical");
+            containerHorizontal.appendChild(containerVertical);
+        }
     }
 }
+createGrid();
 
 // Change the color of the "pixel" when passing the mouse over it.
 const pixelBoxes = document.querySelectorAll(".container-vertical");
@@ -28,25 +31,28 @@ pixelBoxes.forEach(pixelBox =>
     event.target.style.backgroundColor = "black";
 }));
 
+// Create a button that pops a box up to prompt the user for a grid size.
 btnPopup.addEventListener("click", function() {
     popupBox.style.visibility = "visible";
     const btnCancel = document.querySelector("#cancel");
     btnCancel.addEventListener("click", function() {
         popupBox.style.visibility = "hidden";
-        pixelNumber = "";
+        pixelNumber = 0;
         console.log(pixelNumber);
     });
     const input = document.querySelector("input");
     input.addEventListener("input", function(event) {
-        pixelNumber = event.target.value;
+        pixelNumber = Number(event.target.value);
         input.addEventListener("keydown", function(event){
             if (event.key === "Enter") {
+                createGrid();
                 popupBox.style.visibility = "hidden";
                 console.log(pixelNumber);
             };      
         });
 
         console.log(pixelNumber);
+        console.log(typeof(pixelNumber));
     });
 });
 
